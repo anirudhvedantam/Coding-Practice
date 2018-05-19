06ab3777f793f0ff1e74a93e2616097b563b8e32
anirudhvedantam <anirudhvedantam@gmail.com>
2018-05-19 18:20:51 +0530

    Initial Commit

---
+++ Coding-Pratice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Coding-Pratice
+//
+//  Created by Anirudh Vedantam on 19/05/18.
+//  Copyright © 2018 Anirudh Vedantam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
